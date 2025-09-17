2cb31662760e558ce5c64b24174dcd291626800c
akhilnjcet <akhilnjcet@gmail.com>
2025-09-17 14:32:16 +0530

    Add all.java

---
+++ all.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{ Scanner sc=new Scanner(System.in);
+	System.out.println("Enter your mark : ");
+	int mark=sc.nextInt();
+	if(mark>=18){
+	    System.out.println("Eligible for degree");
+	}else{
+	    System.out.println("Better luck next time...");
+	}
+		
+
+	}
+}
